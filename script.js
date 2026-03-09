19c30b17017c07980be9aa25e0176dd232d2def2
zhou.cat@northeastern.edu <150718110+zhou-cat@users.noreply.github.com>
2026-03-09 11:56:42 -0400

    updated script.js with correct paths

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const pressureBtn = document.getElementById("pressuremapBtn");
-    const heatmapBtn = document.getElementById("heatmapBtn");
-    const footanimeBtn = document.getElementById("footanimeBtn");
-  
-    if (pressureBtn) {
-      pressureBtn.addEventListener("click", () => {
-        window.open("../pages/pressure.html", "_blank");
-      });
-    }
-  
-    if (heatmapBtn) {
-      heatmapBtn.addEventListener("click", () => {
-        window.open("../pages/heat.html", "_blank");
-      });
-    }
-  
-    if (footanimeBtn) {
-      footanimeBtn.addEventListener("click", () => {
-        window.open("../pages/foot.html", "_blank");
-      });
-    }
-  
-  });
-  
+  const pressureBtn = document.getElementById("pressuremapBtn");
+  const heatmapBtn = document.getElementById("heatmapBtn");
+  const footanimeBtn = document.getElementById("footanimeBtn");
+
+  if (pressureBtn) {
+    pressureBtn.addEventListener("click", () => {
+      window.open("../pages/pressure.html", "_blank");
+    });
+  }
+
+  if (heatmapBtn) {
+    heatmapBtn.addEventListener("click", () => {
+      window.open("../pages/heat.html", "_blank");
+    });
+  }
+
+  if (footanimeBtn) {
+    footanimeBtn.addEventListener("click", () => {
+      window.open("../pages/foot.html", "_blank");
+    });
+  }
+
+});
